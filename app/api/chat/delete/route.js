5fc131b9be6df3d89dcfebe396ebf57147a389a2
Keylan King <kingkey0101@outlook.com>
2025-07-09 18:28:14 -0500

    added logic for delete chat function

---
+++ app/api/chat/delete/route.js
@@ -0,0 +1,27 @@
+import connectDB from "@/app/config/db";
+import Chat from "@/app/models/Chat";
+import { getAuth } from "@clerk/nextjs/dist/types/server";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  try {
+    const { userId } = getAuth(req);
+    //getting chatId
+    const { chatId } = await req.json();
+
+    if (!userId) {
+      return NextResponse.json({
+        success: false,
+        message: "User not authenticated",
+      });
+    }
+    //deleting chat from mongodb
+    await connectDB();
+    await Chat.deleteOne({ _id: chatId, userId });
+
+    return NextResponse.json({ success: true, message: "Chat Deleted" });
+    
+  } catch (error) {
+    return NextResponse.json({ success: false, erro: error.message });
+  }
+}
