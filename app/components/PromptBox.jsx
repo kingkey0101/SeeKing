3c22419618342fb07c1135da33928f666ece2c60
Keylan King <kingkey0101@outlook.com>
2025-07-09 19:49:27 -0500

    addid logic to sending prompt to ai function

--- app/components/PromptBox.jsx
+++ app/components/PromptBox.jsx
@@ -1,9 +1,95 @@
 import { assets } from "@/assets/assets";
 import Image from "next/image";
 import React, { useState } from "react";
+import { useAppContext } from "../context/AppContext";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const PromptBox = ({ setIsLoading, isLoading }) => {
   const [prompt, setPrompt] = useState("");
+  const { user, chats, setChats, selectedChat, setSelectedChat } =
+    useAppContext();
+
+  const sendPrompt = async (e) => {
+    const promptCopy = prompt;
+    try {
+      e.preventDefault();
+      if (!user) return toast.error("Login to use SeeKing");
+      if (isLoading) return toast.error("Please wait for previous response");
+      setIsLoading(true);
+      setPrompt("");
+
+      const userPrompt = {
+        role: "user",
+        content: prompt,
+        timestamp: Date.now(),
+      };
+      //saving prompts in array
+      setChats((prevChats) =>
+        prevChats.map((chat) =>
+          chat._id === selectedChat._id
+            ? {
+                ...chat,
+                messages: [...chat.messages, userPrompt],
+              }
+            : chat
+        )
+      );
+
+      //save prompt in current chat
+      setSelectedChat((prev) => ({
+        ...prev,
+        messages: [...prev.messages, userPrompt],
+      }));
+
+      const { data } = await axios.post("/api/chat/ai", {
+        chatId: selectedChat._id,
+        prompt,
+      });
+      if (data.success) {
+        setChats((prevChats) =>
+          prevChats.map((chat) =>
+            chat._id === selectedChat._id
+              ? { ...chat, messages: [...chat.messages, data.data] }
+              : chat
+          )
+        );
+
+        const message = data.data.content;
+        const messageTokens = message.split(" ");
+        let assistantMessage = {
+          role: "assistant",
+          content: "",
+          timestamp: Date.now(),
+        };
+        setSelectedChat((prev) => ({
+          ...prev,
+          messages: [...prev.messages, assistantMessage],
+        }));
+
+        for (let i = 0; i < messageTokens.length; i++) {
+          setTimeout(() => {
+            assistantMessage.content = messageTokens.slice(0, i + 1).join(" ");
+            selectedChat((prev) => {
+              const updatedMessages = [
+                ...prev.messages.slice(0, -1),
+                assistantMessage,
+              ];
+              return { ...prev, messages: updatedMessages };
+            });
+          }, i * 100);
+        }
+      } else {
+        toast.error(data.message);
+        setPrompt(promptCopy);
+      }
+    } catch (error) {
+      toast.error(error.message);
+      setPrompt(promptCopy);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <form
       className={`w-full ${false ? "max-w-3xl" : "max-w-2xl"}
